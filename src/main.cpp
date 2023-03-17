4d581aae73721de53735f16401aa9c720610a11a
miralouisepaquin <90649621+miralouisepaquin@users.noreply.github.com>
2023-03-17 12:54:33 -0400

    Modification du main.cpp
    Logique applicative de base en sub et pub avec l'allumage d'un Leds lorsque le le bon message arrive.
    Fermeture du Leds avec un button et envoi du code (static pour le moment)

--- src/main.cpp
+++ src/main.cpp
@@ -19,18 +19,30 @@ String ssIDRandom;
 // MQTT client
 WiFiClient wifiClient;
 PubSubClient mqttClient(wifiClient); 
-char *mqttServer = "broker.hivemq.com";
+const char *mqttServer = "172.16.5.100";
 int mqttPort = 1883;
-const char *topic = "zigbee2mqtt";
+const char *topic = "zigbee2mqtt/askCode01";
+
+//Pour la gestion des boutons
+#include "MyButton.h"
+MyButton *myButtonAction = NULL;
+MyButton *myButtonReset = NULL;
 
 //Définition des trois leds de statut
-#define GPIO_PIN_LED_LOCK_ROUGE 12
+#define GPIO_PIN_LED_LOCK_ROUGE 23
 
+//Deinition des PIN pour la connexion serie
+#define RXD2 18
+#define TXD2 19
 //Variable pour vitesse baud Serial
-#define BAUD 9600
+#define BAUD_RATE 115200
 
 //Variable pour le code (Pin) entré
 std::string code = "1234";
+//Variable pour le button Enter
+bool buttonEnter = 0;
+//Variable pour le message du subscribe
+String messageTemp;
 
 void connectToWiFi() {
   String ssIDRandom, PASSRandom;
@@ -42,18 +54,16 @@ void connectToWiFi() {
     PASSRandom = PASSWORD;
     PASSRandom = PASSRandom + stringRandom;
 
+    char strToPrint[128];
+    sprintf(strToPrint, "Identification : %s   MotDePasse: %s", ssIDRandom, PASSRandom);
+    Serial.println(strToPrint);
+
     if (!wm.autoConnect(ssIDRandom.c_str(), PASSRandom.c_str())){
       Serial.println("Erreur de connexion.");
       }
     else {
       Serial.println("Connexion Établie:");
       }
-}
-
-void setupMQTT() {
-  mqttClient.setServer(mqttServer, mqttPort);
-  // set the callback function
-  mqttClient.setCallback(callback);
 }
 
 void reconnect() {
@@ -75,45 +85,63 @@ void callback(char* topic, byte* message, unsigned int length) {
   Serial.print("Message arrived on topic: ");
   Serial.print(topic);
   Serial.print(". Message: ");
-  String messageTemp;
   
   for (int i = 0; i < length; i++) {
     Serial.print((char)message[i]);
     messageTemp += (char)message[i];
-  }
-  Serial.println();
+    }
 
   // Valide le message entrant pour la demande du code de l'alarme
-  if (String(topic) == "zigbee2mqtt/askCode01") {
-    if(messageTemp == "on"){
-      Serial.println("Veuillez entrer le PIN code pour arrêter l'alarme!");
+  if (strcmp(topic,"zigbee2mqtt/askCode01")==0) {
+    delay(100);
+    if(strcmp(messageTemp.c_str(),"on")==0 && digitalRead(GPIO_PIN_LED_LOCK_ROUGE)==false){
+      Serial.println("\nVeuillez entrer le PIN code pour arrêter l'alarme!");
       digitalWrite(GPIO_PIN_LED_LOCK_ROUGE, HIGH);
     }
-    else if(messageTemp == "off"){
-      Serial.println("Code Ok!");
-      digitalWrite(GPIO_PIN_LED_LOCK_ROUGE, LOW);
-    }
   }
+}
+
+void setupMQTT() {
+  mqttClient.setServer(mqttServer, mqttPort);
+  // set the callback function
+  mqttClient.setCallback(callback);
 }
 
 void setup() {
   Serial.begin(9600);
   connectToWiFi();
   setupMQTT();
+
+  //Gestion des boutons
+  myButtonAction = new MyButton();        //Pour lire le bouton actions
+  myButtonAction->init(T4);
+  int sensibilisationButtonAction = myButtonAction->autoSensibilisation();
+
+  Serial.print("sensibilisationButtonAction : "); Serial.println(sensibilisationButtonAction);
+
   pinMode(GPIO_PIN_LED_LOCK_ROUGE, OUTPUT);
 }
 
 void loop() {
-  if (!mqttClient.connected())
-    reconnect();
+  if (!mqttClient.connected()){
+    reconnect();    
+    }    
   mqttClient.loop();
+
+  //Gestion du bouton Action (remplace le enter du Stone)
+  int buttonAction = myButtonAction->checkMyButton();
+  if(buttonAction > 2  && digitalRead(GPIO_PIN_LED_LOCK_ROUGE)==true)  {  //Si appuyé plus de 0.2 secondes
+      buttonEnter = 1;
+      }
+
+  delay(100);
   //Si le bouton "Enter" est peser sur le Stone = Publish le code
-  if(){
-    if(){
-      mqttClient.publish(topic, code.c_str());
-    }
-    else{
-      Serial.print("");
-    }
+  if(buttonEnter == 1){
+    mqttClient.publish(topic, code.c_str());
+    digitalWrite(GPIO_PIN_LED_LOCK_ROUGE, LOW);
+    buttonEnter = 0;
   }
+
+  messageTemp="";
+  delay(10);
 }
